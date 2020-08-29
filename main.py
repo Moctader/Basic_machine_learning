5e3916ca3898268a11b12e2f197b64ab53d34466
Moctader <moctader.golam@gmail.com>
2020-08-29 09:09:22 +0300

    changed

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
-k=1
-print(k)
+import numpy as np 
+import pandas as pd 
+#import keras
+import os
+from tqdm import tqdm
+
+for i in tqdm(range(int(9e6))):
+    pass
+
+# Build function
+def function1 ():
+    print('áhh')
+    print('ahh2')
+print('this function is outside of this era')
+function1()
+function1()
+
+
+# function declaration
+def function2(x):
+    return 2*x
+
+a=function2(3)
+b=function2(4)
+print(a)
+print(b)
+
+def function3 (x,y):
+    return x+y
+
+j=function3(1,2)
+print(j)
+
+print('jokes')
+
+for i in tqdm(10):
+    print(i)
