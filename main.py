a25d59cfda5b4702a04048135b5252fb00a2f3c2
Moctader <moctader.golam@gmail.com>
2020-09-09 23:44:01 +0300

    Titanic

--- main.py
+++ main.py
@@ -1,49 +1,44 @@
-import numpy as np 
-import pandas as pd 
-#import keras
-import os
-from tqdm import tqdm
+import matplotlib as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
-import pyautogui
-import time
-message=10
-while message>0:
-    time.sleep(4)
-    pyautogui.typewrite('i need you.')
-    time.sleep(2)
-    pyautogui.press('enter')
-    message =message-1
+titanic =pd.read_csv('C:/Users/golam/PycharmProjects/Cross_validation/titanic.csv')
+titanic.head()
+print(titanic.head())
 
-for i in tqdm(range(int(9e6))):
-    pass
+# missing value
+titanic.isnull().sum()
+#print(titanic.isnull().sum())
 
-# Build function
-def function1 ():
-    print('áhh')
-    print('ahh2')
+# Missing value is filled with average value
+titanic['Age'].fillna(titanic['Age'].mean(), inplace=True)
+#print(titanic['Parch'].head(10))
 
-#print('this function is outside of this era')
-function1()
-function1()
+# Combine Sibsp & parch
 
+# for i, col in enumerate(['SibSip', 'Parch']):
+#     plt.figure(i)
+#     sns.catplot(x=col, y='Survived', data=titanic, kind='point', aspect=2, )
 
-# function declaration
-def function2(x):
-    return 2*x
+# Combine SibSp &  Parch
+titanic['Family_cnt'] = titanic['SibSp'] + titanic['Parch']
 
-a=function2(3)
-b=function2(4)
-print(a)
-print(b)
+titanic.drop(['PassengerId', 'SibSp', 'Parch'], axis=1, inplace=True)
+print(titanic.head(5))
+titanic.isnull().sum()
 
-def function3 (x,y):
-    return x+y
+# Applying groupby method
+titanic.groupby(titanic['Cabin'].isnull())['Survived'].mean()
+titanic['Cabin_ind'] = np.where(titanic['Cabin'].isnull(), 0,1) # if missing value Then 0 else 1
+print(titanic['Cabin_ind'].head(5))
 
-j=function3(1,2)
-print(j)
+# Identifying male and female by binary number
+gender_num = {'male': 0, 'female': 1}
+titanic['Sex'] = titanic['Sex'].map(gender_num) # Replaycing mal and female with 0 a nd 1
 
-print('jokes')
+# Drop the unnecessary variable
+titanic.drop(['Cabin', 'Embarked', 'Name', 'Ticket'], axis=1, inplace=True)
 
-for i in tqdm(10):
-    print(i)
-# Change
+# Cleaned dataset save for further use
+titanic.to_csv('C:/Users/golam/PycharmProjects/Cross_validation/titanic_cleaned.csv')
